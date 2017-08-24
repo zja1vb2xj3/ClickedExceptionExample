142b19508617a5d3d5ead556accd70715d9e6e77
PDG <zja1vb2xj3@naver.com>
2017-08-25 03:27:33 +0900

    nullPointerExceptionOccur ArrayIndexOutOfBoundsExceptionOccur NumberFormatExceptionOccur ClassCastExceptionOccur 메서드 생성

--- app/src/main/java/com/example/user/clickedexceptionexample/MainActivity.java
+++ app/src/main/java/com/example/user/clickedexceptionexample/MainActivity.java
@@ -25,41 +25,57 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        exceptionButton = (Button)findViewById(R.id.exceptionButton);
+        exceptionButton = (Button) findViewById(R.id.exceptionButton);
 
-        exceptionButton.setOnClickListener(v -> exceptionButtonClick(null));
+        exceptionButton.setOnClickListener(v -> exceptionButtonClick());
     }
 
     /**
      * exceptionButton을 Click하게되면 작동되는 메서드 입니다.
      * Exception 작동 주석 Head를 보시려면 @ExceptionHead 태그를 검색하세요
      */
-    private <T>void exceptionButtonClick(T value) {
-        try{
-
+    private void exceptionButtonClick() {
+        try {
 //            @ExceptionHead NullPointerException
-//            System.out.println(value.toString());
+//            nullPointerExceptionOccur(null);
 
 //            @ExceptionHead ArrayIndexOutOfBoundsException
-//            int[] strValues = new int[0];
-//            strValues[0] = 0;
+//            ArrayIndexOutOfBoundsExceptionOccur();
 
 //            @ExceptionHead NumberFormatException
-//            String beforeChangeStr = "abc";
-//            int afterChangeInt = Integer.parseInt(beforeChangeStr);
-//            System.out.println(afterChangeInt);
+//            NumberFormatExceptionOccur("abc");
 
+//            @ExceptionHead ClassCastException
+//            ClassCastExceptionOccur();
 
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             Toast.makeText(getApplicationContext(), "NullPointerException", Toast.LENGTH_LONG).show();
-        }
-        catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             Toast.makeText(getApplicationContext(), "ArrayIndexOutOfBoundsException", Toast.LENGTH_LONG).show();
-        }
-        catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             Toast.makeText(getApplicationContext(), "NumberFormatException", Toast.LENGTH_LONG).show();
+        } catch (ClassCastException e) {
+            Toast.makeText(getApplicationContext(), "ClassCastException", Toast.LENGTH_LONG).show();
         }
+    }//end exceptionButtonClick
 
+    private <T> void nullPointerExceptionOccur(T value) {
+        System.out.println(value.toString());
     }
+
+    private void ArrayIndexOutOfBoundsExceptionOccur() {
+        int[] strValues = new int[0];
+        strValues[0] = 0;
+    }
+
+    private void NumberFormatExceptionOccur(String beforeChangeStr) {
+        int afterChangeInt = Integer.parseInt(beforeChangeStr);
+    }
+
+    private void ClassCastExceptionOccur() {
+        Exception exception = new Exception();
+        NullPointerException nullPointerException = (NullPointerException) exception;
+    }
+
+
 }
